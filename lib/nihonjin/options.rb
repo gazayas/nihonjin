075a6f303510e487784c0965e8786faa6fb05085
gazayas <g-zayas@hotmail.com>
2016-12-17 15:56:07 -0600

    options.rb

--- lib/nihonjin/options.rb
+++ lib/nihonjin/options.rb
@@ -1,10 +1,34 @@
 module Nihonjin
 
   EncodingTypes = {
-        utf_8: '-w',
-        shift_jis: '-s',
-        iso_2022_jp: '-j',
-        euc: '-e'
-      }
+    iso_2022_jp: '-j',
+    shift_jis: '-s',
+    euc: '-e',
+    utf_8: '-w',
+    UTF_16BE: '-w16',
+    UTF_32BE: '-w32'
+  }
+
+  InputCode = {
+    iso_2022_jp: '-J',
+    shift_jis: '-S',
+    euc: '-E',
+    utf_8: '-W',
+    UTF_16BE: '-W16',
+    UTF_32BE: '-W32'
+  }
+
+  NewLineMode = {
+    unix: '-Lu',
+    windows: '-Lw',
+    mac: '-Lm'
+  }
+
+  Systems = {
+    unix: '--unix',
+    mac: '--mac',
+    msdos: '--msdos',
+    windows: '--windows'
+  }
 
 end
